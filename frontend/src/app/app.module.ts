2cd979639622464ab1480515706dce2438850319
lucasalcantarap <lucaspe0685@gmail.com>
2021-04-06 11:51:47 -0300

    criado component product-read

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -23,6 +23,7 @@ import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
 
 /**
  * Componentes, diretivas e pipes
@@ -36,6 +37,7 @@ import { MatInputModule } from "@angular/material/input";
     HomeComponent,
     ProductCrudComponent,
     ProductCreateComponent,
+    ProductReadComponent,
   ],
   imports: [
     BrowserModule,
